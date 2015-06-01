df6e45947aaab9776a1197cb63fca18b71612f57
Chris Perry <byndcivilization@gmail.com>
2015-06-01 14:49:28 -0400

    edited transform script for eiti-blog-post data

--- eiti-blog-post/data/transform.py
+++ eiti-blog-post/data/transform.py
@@ -5,43 +5,6 @@ import csv
 import sys
 import pycountry
 
-# Report ID
-# Years Covered
-# Country Name
-# Commodity
-# Payments Received by Government from Covered Companies (US$)
-# Payments Made by Covered Companies (US$)
-# Total GDP US$ nominal
-# Total GDP US$ nominal
-# Population,Extractive revenues (% GDP)
-# "Revenue, excluding grants (% GDP)"
-# "Revenue, excluding grants"
-# Total government revenues (US$)
-# Extractive revenues (%  revenues excluding grants)
-# "Extractive exports (US$), UNCTAD"
-# "Total exports (US$), UNCTAD"
-# Extractive exports (% total exports)
-# ODA (US$)
-# Extractive revenues (% ODA)
-# "Health expenditure, public (% of GDP)"
-# Health expenditure (US$)
-# Extractive revenues (% health budget)
-# Public spending on education (% of GDP) 
-# Public spending (US$)
-# Extractive revenues (% education budget)
-# TI Corruption Perception Index
-# Poverty Headcount (%pop) - WB
-# Life expectancy at birth - WB
-# Infant mortality - WB
-# Primary school enrolment - WB
-# RGI score (2010)
-# RGI score (2013)
-# Voice and Accountability WGI score
-# Political Stability WGI score
-# Government Effectiveness WGI score
-# Regulatory Quality WGI score
-# Control of Corruption WGI score
-
 def main(args):
     """ main function """
     with open('./eitidata.csv', 'rU') as f:
@@ -57,20 +20,16 @@ def main(args):
         file_name = args[1].lower()
 
         current_report = 'none'
-        year_list = {}
         json_data = []
 
         for row in csv_reader:
             if row[0] == current_report:
                 pass
             else:
-                year = row[lkey['Years Covered']]
-                country = row[lkey['Country Name']]
+                year = row[lkey['Year']]
+                country = row[lkey['Country']]
 
-                if country == "C\x93te d'Ivoire":
-                    country = "Cote d'Ivoire"
-                    country_id = 'CIV'
-                elif country == "C\xd2te d'Ivoire":
+                if country == "C\x93te d'Ivoire" or country == "C\xd2te d'Ivoire" or country == "C\xf4te d'Ivoire":
                     country = "Cote d'Ivoire"
                     country_id = 'CIV'
                 elif country == 'Democratic Republic of Congo':
@@ -83,70 +42,28 @@ def main(args):
                     country_id = 'TZA'
                 else:
                     country_id = pycountry.countries.get(name=country).alpha3
-                # print country + ' ' + year
 
-                resource = float(row[lkey['Payments Received by Government from Covered Companies (US$)']])
+                resource = float(row[lkey['Total extractive government receipts (US$)']])
                 if row[lkey[comp]] == '' or row[lkey[comp]] == '..' or row[lkey[comp]] == '#N/A':
                     comp_val = 0
                 else:
                     comp_val = float(row[lkey[comp]])
 
-                if year not in year_list:
-                    json_data.append({
-                        "name": "*Total",
-                        "id": 'all',
-                        "year": year,
-                        "cats": [
-                            {
-                                "name": "Resource revenues (US$)",
-                                "value": resource
-                            },
-                            {
-                                "name": comp,
-                                "value": comp_val
-
-                            }
-                        ]
-                    })
-                    year_list[year] = len(json_data)-1
-                    json_data.append({
-                        "name": country,
-                        'id': country_id,
-                        "year": year,
-                        "cats": [
-                            {
-                                "name": "Resource revenues (US$)",
-                                "value": resource
-                            },
-                            {
-                                "name": comp,
-                                "value": comp_val
-
-                            }
-                        ]
-                    })
-                else:
-                    json_data.append({
-                        "name": country,
-                        'id': country_id,
-                        "year": year,
-                        "cats": [
-                            {
-                                "name": "Resource revenues (US$)",
-                                "value": resource
-                            },
-                            {
-                                "name": comp,
-                                "value": comp_val
-
-                            }
-                        ]
-                    })
-                    json_data[year_list[year]]["cats"][0]["value"] += resource
-                    json_data[year_list[year]]["cats"][1]["value"] += comp_val
-
-
-
+                json_data.append({
+                    "name": country,
+                    'id': country_id,
+                    "year": year,
+                    "cats": [
+                        {
+                            "name": "Resource revenues (US$)",
+                            "value": resource
+                        },
+                        {
+                            "name": comp,
+                            "value": comp_val
+                        }
+                    ]
+                })
                 current_report = row[0].strip()
 
         # write out archive of update into archive folder
